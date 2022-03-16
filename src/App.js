c8a1029a7c6e655399bdea326ef949d16634368c
Rasmus <reachrasmus@gmail.com>
2022-03-16 17:07:34 +0100

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react¨
 import  Picker from 'emoji-picker-react'
-import "./App.css";
 import styled from 'styled-components'
+import "./App.css"
 
 const Container = styled.div`
-  display: grid;
-  gap: 3vh;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
   margin: 10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: 3vh;
 `
 
 function Box({ children, k, disableCounter, ...props }) {
-  const [counter, setCount] = useState([]);
   const [showSettings, setShowSettings] = useState(false)
+  const [counter, setCount] = useState([]);
   return (
     showSettings ? <div>
       <button onClick={() => setShowSettings(false)}>...</button>
@@ -40,37 +40,34 @@ function Box({ children, k, disableCounter, ...props }) {
         </>
       )}
     </div>
-  );
+  )
 }
 
 const StyledBox = styled(Box)`
   outline: solid black 1px;
   color: white;
   font-weight: bold;
-  padding: 1vh ;
-  background: lightslategrey ;
-  font-size: 5vh ;
+  padding: 1vh;
+  background: lightslategrey;
+  font-size: 5vh;
   display: grid;
   gap: 1vh;
 `
+
 function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback
+  const savedCallback = useRef()
   useEffect(() => {
-      savedCallback.current = callback;
+      savedCallback.current = callback
   }, [callback]);
-
-  // Set up the interval
   useEffect(() => {
       function tick() {
-          savedCallback.current();
+          savedCallback.current()
       }
       if (delay !== null) {
-          let id = setInterval(tick, delay);
-          return () => clearInterval(id);
+          let id = setInterval(tick, delay)
+          return () => clearInterval(id)
       }
-  }, [delay]);
+  }, [delay])
 }
 
 function add0(num){
@@ -101,30 +98,28 @@ function DateNow(){
 }
 
 export default function App() {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [boxes, setBoxes] = useState([]);
   const onEmojiClick = (e, obj) => {
     setBoxes([...boxes, { content: obj.emoji }])
     showEmojiPicker ? setShowEmojiPicker(false) : setShowEmojiPicker(true)
-  };
-  const [boxes, setBoxes] = useState([]);
-  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  }
   return (
     <>
-    <DateNow/>
-    <Container>
-      {boxes.map(({ content }, i) => (
-        <StyledBox key={i}>
-          {content}
-        </StyledBox>
-      ))}
-      { showEmojiPicker 
-        ? <Picker onEmojiClick={onEmojiClick}/> 
-        : <StyledBox disableCounter={true} onClick={() => { setShowEmojiPicker(true) }
-      }
-      > + </StyledBox> }
-    </Container>
+      <DateNow/>
+      <Container>
+        { boxes.map(({ content }, i) => (
+          <StyledBox key={i}>
+            {content}
+          </StyledBox>
+        )) }
+        { showEmojiPicker 
+          ? <Picker onEmojiClick={onEmojiClick}/> 
+          : <StyledBox disableCounter={true} onClick={
+            () => { setShowEmojiPicker(true) }
+        }
+        > + </StyledBox> }
+      </Container>
     </>
-  );
-
-
-
+  )
 }
