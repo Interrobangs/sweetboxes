e01930eec4af8b87f49fbf1ad79e4af270db774a
Rasmus <reachrasmus@gmail.com>
2022-03-17 14:14:14 +0100

    changed date

--- src/App.js
+++ src/App.js
@@ -75,14 +75,31 @@ function add0(num){
 }
 
 function getDateNow(){
-  const d = new Date()
-  const month = d.getMonth() + 1
-  const hours = add0(d.getHours())
-  const minuts = add0(d.getMinutes())
-  const seconds = add0(d.getSeconds())
-  const dateNow = [d.getFullYear(), month, d.getDate()].join('/')
-  const time = [hours, minuts, seconds].join(':')
-  return  dateNow + ' ' + time
+  const date = new Date()
+  // request a weekday along with a long date
+  var options = { 
+     weekday: 'long',
+     year: 'numeric',
+     month: 'long', 
+     day: 'numeric',
+     hour: 'numeric', 
+     minute: 'numeric', 
+     second: 'numeric'
+    };
+  const thisDate = new Intl.DateTimeFormat(navigator.language, options).format(date).split(',').join('')
+  const capitalizeFirstLetter = ([ first, ...rest ], locale = navigator.language) =>
+  first.toLocaleUpperCase(locale)
+
+  return thisDate[0].toUpperCase() + thisDate.substring(1, thisDate.length)
+  // const thisMoment = moment().format('DD MMMM YYYY hh:mm:ss');
+  // const d = new Date()
+  // const month = d.getMonth() + 1
+  // const hours = add0(d.getHours())
+  // const minuts = add0(d.getMinutes())
+  // const seconds = add0(d.getSeconds())
+  // const dateNow = [d.getFullYear(), month, d.getDate()].join('/')
+  // const time = [hours, minuts, seconds].join(':')
+  // return  thisMoment
 }
 
 function DateNow(){
