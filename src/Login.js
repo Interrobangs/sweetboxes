9842c9261a6d8bf09c7363480b35484544438697
Rasmus <reachrasmus@gmail.com>
2022-03-21 05:08:17 +0100

    removed unused imports

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,7 @@
 import { signInWithPopup } from "firebase/auth"
-import { addDoc } from "firebase/firestore";
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
- import { collection } from "firebase/firestore"; 
-import { auth, db, provider} from "./Firebase";
+import { auth, provider} from "./Firebase";
 const Login = () => {
     const navigate = useNavigate()
     const [authing, setAuthing] = useState(false)
@@ -11,7 +9,7 @@ const Login = () => {
     const signInWithGoogle = async () => {
         setAuthing(true)
         signInWithPopup(auth, provider)
-        .then((response) => {
+        .then(() => {
         // const docRef = collection(db, 'users').doc(response.user.uid);
         // auth.currentUser.getIdToken().then((idToken) => {  // <------ Check this line
         //     console.log(1)
@@ -42,7 +40,7 @@ const Login = () => {
         //     born: 1815
         // });
         
-        navigate('/')
+            navigate('/')
         })
         .catch(error => {
             console.log(error)
